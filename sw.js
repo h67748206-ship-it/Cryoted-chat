cbd71d3d3e2601dc7c3575b959426efb78c7b6ab
h67748206-ship-it <h67748206@gmail.com>
2026-07-25 21:34:19 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE = 'xenity-secure-v1';
+var SHELL = ['./index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(SHELL); }));
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  if(e.request.url.indexOf('ntfy.sh') !== -1) return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).catch(function(){ return caches.match('./index.html'); });
+    })
+  );
+});
